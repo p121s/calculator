0402fc475deb3487c877dd03e6522e446c5c4206
p121s <p121s@mail.ru>
2021-09-14 14:20:18 +0300

    Fix code

--- src/app/modules/calculator.js
+++ src/app/modules/calculator.js
@@ -3,7 +3,7 @@ export default class Calculator {
     constructor(){
         this.result = 0;
         this.memory = 0;
-        this.tempResult = 6;
+        this.tempResult = 0;
     }
 
     execute(){}
